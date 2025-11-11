33a50ac6f39011268aaf46c163e807773fc03e84
sunjingchun <46308451+sunjingchun@users.noreply.github.com>
2025-11-11 15:37:51 +0000

    页面指向自己注册的域名

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // 将 base 改为 "/<repo>/" 以用于 GitHub Pages，例如 "/softblog/"
 export default defineConfig({
 plugins: [react()],
-base: '/my-blog/'
+base: '/'
 })
